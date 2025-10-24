92e2a697c35135d8a3cf987b4960e13de209f432
saophaidihoc <saophaidihoc@gmail.com>
2025-10-25 04:05:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-const nameGirl = 'Em';
+const nameGirl = 'Nguyễn Trần Khánh Linh';
 const giftUrl = 'http://nodemy.vn';
-const eventName = 'Happy Birthday To Linh';
+const eventName = 'Happy Birthday';
 const titleCard = 'From Duc';
 const contentCard = 'Chúc em sinh nhật dui dẻ nhoa. Anh không biết nói gì hơn ngoài wish u all the best! Anh xin lỗi em nhiều nha mong em nhận quà muộn từ anh';
 
